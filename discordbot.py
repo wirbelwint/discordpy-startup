30e73e74f1c976005bb6d85a46c85a7f882e87d8
wirbelwint <78839032+wirbelwint@users.noreply.github.com>
2021-02-10 15:19:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,8 +21,8 @@ async def ping(ctx):
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '15:15':
-        channel = client.get_channel(#雑談)
+    if now == '15:25':
+        channel = client.get_channel(808919318359834624)
         await channel.send('テスト')  
 
 #ループ処理実行
