a2600c42342662742b4ab5b55d77ec00296c9188
wirbelwint <78839032+wirbelwint@users.noreply.github.com>
2021-02-10 16:29:41 +0900

    Create discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,34 +1,16 @@
-from discord.ext import commands
-import os
-import traceback
-
 import discord
 from discord.ext import tasks
 from datetime import datetime 
 
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+TOKEN = "ODA4OTI1Nzc1OTk0MjkwMjM3.YCNo1g.lKAz63_rVsi5wRytNveRLiEAPaE"
 CHANNEL_ID = 808919318359834624 
 client = discord.Client()
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
 
 @tasks.loop(seconds=60)
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '16:25':
+    if now == '16:30':
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('テスト')  
 
